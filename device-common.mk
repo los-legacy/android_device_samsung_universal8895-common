fc3f6999220a89de4972f6c2ccbacfba45a10694
BenLue <BenJule@users.noreply.github.com>
2020-04-19 20:53:32 +0200

    dtbhtoolExynos: PRODUCT_HOST_PACKAGES

--- device-common.mk
+++ device-common.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.samsung
 
 # Flat device tree for boot image
-PRODUCT_HOST_PACKAGES += \
+PRODUCT_PACKAGES += \
     dtbhtoolExynos
 
 # Gatekeeper
